164abb28b89fd498aa50468e49f4849ca454e866
Kyle Wright <kylejamwright@gmail.com>
2020-07-27 11:06:18 -0600

    Included skeleton to UI

---
+++ presentation/src/components/Entree.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+function Entree() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>Hello world</li>
+                <li>Hello world</li>
+                <li>Hello world</li>
+            </ul>
+        </div>
+        
+    );
+}
+
+export default Entree;
