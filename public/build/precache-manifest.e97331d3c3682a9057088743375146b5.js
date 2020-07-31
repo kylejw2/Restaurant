55b5672ab4af0e0c50b714ef100cfe70f362c3e0
Kyle Wright <kylejamwright@gmail.com>
2020-07-31 08:42:34 -0600

    Embedded front end code into the back end code

---
+++ public/build/precache-manifest.e97331d3c3682a9057088743375146b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6349d3336228bb5eb49d8e19de6b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b0668538b0030c6494",
+    "url": "/static/css/main.812fd2c1.chunk.css"
+  },
+  {
+    "revision": "d8c2fc01017c23058081",
+    "url": "/static/js/2.45817c9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45817c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b0668538b0030c6494",
+    "url": "/static/js/main.7dfcceed.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
