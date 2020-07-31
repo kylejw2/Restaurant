05d9d44f6ad5f9b09db3ab4f00f54b3069ad1439
Kyle Wright <kylejamwright@gmail.com>
2020-07-31 11:15:59 -0600

    Ninth attempt to fix Heroku errors

--- public/precache-manifest.018f98466bd0c7abc148ad42d790ba0e.js
+++ public/precache-manifest.018f98466bd0c7abc148ad42d790ba0e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbe34eda47169a5be4334ed0e11204a0",
+    "revision": "b1cd0a02e9f94f58f6a4fe0319deb270",
     "url": "/index.html"
   },
   {
-    "revision": "0badff6c7b73a1e423b8",
+    "revision": "71e8a76e83cd5500235a",
     "url": "/static/css/main.812fd2c1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.45817c9a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0badff6c7b73a1e423b8",
-    "url": "/static/js/main.1b2f10c1.chunk.js"
+    "revision": "71e8a76e83cd5500235a",
+    "url": "/static/js/main.614c745d.chunk.js"
   },
   {
     "revision": "1aadcde5ef86e7287682",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6a392f9f6ab2c59620c2eff2b5b728e.js"
+  "/precache-manifest.018f98466bd0c7abc148ad42d790ba0e.js"
 );
 
 self.addEventListener('message', (event) => {
